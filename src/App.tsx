594284738dbe95e720b2ca71aaa49297652f26ae
karan <karan14verma@gmail.com>
2018-11-19 20:05:13 -0500

    initial commit, added my own component

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import './App.css';
+import ExampleItem from './components/ExampleItem';
+
+import logo from './logo.svg';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Hello World</h1>
+        </header>
+        <ExampleItem/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ExampleItem.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class ExampleItem extends React.Component {
+  public render() {
+    return (
+      <div>
+          <h2>This is a new component</h2>
+      </div>
+    );
+  }
+}
+
+export default ExampleItem;
