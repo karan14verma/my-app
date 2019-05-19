6e7f0d1fe2aedf6e8f3027b8ed4acc82694eb7de
Karan Verma <karan14verma@gmail.com>
2019-05-19 15:45:45 -0400

    reordered project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import './App.css';
-import ExampleItem from './components/ExampleItem';
+import './css/App.css';
 
+import CardContainer from './Card/Containers/CardContainer';
 import logo from './logo.svg';
 
 class App extends React.Component {
@@ -12,7 +12,7 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Hello World</h1>
         </header>
-        <ExampleItem/>
+        <CardContainer/>
       </div>
     );
   }
---
+++ src/Card/Containers/CardContainer.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Card from '../Components/Card';
+
+class CardContainer extends React.Component {
+    public render() {
+      return (
+        <Card />
+      );
+    }
+}
+
+export default CardContainer;
