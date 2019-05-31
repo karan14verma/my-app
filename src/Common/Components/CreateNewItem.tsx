54091bccdb9a16a19ff77b51a512e3fcf5e13582
Karan Verma <karan14verma@gmail.com>
2019-05-30 20:50:15 -0400

    added CreateNewItem component

---
+++ src/Common/Components/CreateNewItem.tsx
@@ -0,0 +1,29 @@
+import Input from '@material-ui/core/Input';
+
+import * as React from 'react';
+
+interface IProps{
+    defaultValue: string;
+}
+
+// interface IState{
+// }
+
+class CreateNewItem extends React.Component<IProps> {
+
+    public render() {
+        return (
+            <div className={'container'}>
+                 <Input
+                    defaultValue= {this.props.defaultValue}
+                    className={'input'}
+                    inputProps={{
+                        'aria-label': 'Description',
+                    }}
+      />
+      </div>
+    );
+  }
+}
+
+export default CreateNewItem;
