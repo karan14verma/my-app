d382f98342e1d88262ba605f7a3ac6c40fb3aee6
Karan Verma <karan14verma@gmail.com>
2019-06-13 14:50:35 -0400

    the changes

--- src/Card/Containers/CardContainer.tsx
+++ src/Card/Containers/CardContainer.tsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 import CardComponent from '../Components/CardComponent';
 
 class CardContainer extends React.Component {
-  // all the logic stays in the containers
+  // all the redux logic stays in the containers
+  // list items should come from redux
     public render() {
       return (
         <CardComponent
+          listItems= {["el1", "el2", "el3"]}
         />
       );
     }
