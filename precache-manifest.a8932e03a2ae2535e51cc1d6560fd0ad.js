30c6b9ab41262dc3a571bdf9fe505594f3b3ff01
Karan Verma <karanverma@Karans-MacBook-Pro.local>
2019-09-05 20:59:45 -0400

    Updates

---
+++ precache-manifest.a8932e03a2ae2535e51cc1d6560fd0ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "ef3272c334ced2cd65c9",
+    "url": "/my-app/static/js/main.ef3272c3.chunk.js"
+  },
+  {
+    "revision": "58825b089f4e3d0202f7",
+    "url": "/my-app/static/js/1.58825b08.chunk.js"
+  },
+  {
+    "revision": "ef3272c334ced2cd65c9",
+    "url": "/my-app/static/css/main.ede12b04.chunk.css"
+  },
+  {
+    "revision": "ccd0954d7665104ed29c94d2cef246c6",
+    "url": "/my-app/index.html"
+  }
+];
